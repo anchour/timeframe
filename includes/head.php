92213d19ed6a1c85bbc9e3e47f5eb59324b6f1f2
Matthew Robitaille <matt@anchour.com>
2014-05-20 09:51:07 -0400

    Add typekit fonts and update base font.
    Also added mixins file that is currently unused.

--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,8 @@
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   <meta name="viewport" content="width=device-width, user-scalable=no" />
   <link rel="stylesheet" href="assets/css/styles.min.css">
+
+  <script type="text/javascript" src="//use.typekit.net/kom1ckp.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
