c7408ecafd7f9591b75f9a2b56150d87e482ec88
Matthew Robitaille <matt@anchour.com>
2014-05-20 17:32:40 -0400

    add analytics

--- includes/head.php
+++ includes/head.php
@@ -9,5 +9,15 @@
   <script type="text/javascript" src="//use.typekit.net/kom1ckp.js"></script>
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <!--[if lt IE 9]><script src="assets/js/ie.js"></script><![endif]-->
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-51166348-1', 'tendavid.com');
+    ga('send', 'pageview');
+  </script>
 </head>
 <body>
