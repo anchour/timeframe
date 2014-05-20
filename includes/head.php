939a14e3e807939bcae33926f297260fd166035a
Matthew Robitaille <matt@anchour.com>
2014-05-20 14:06:34 -0400

    Update title.

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Baseplate</title>
+  <title>TimeFrame</title>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
   <link rel="stylesheet" href="assets/css/styles.min.css">
